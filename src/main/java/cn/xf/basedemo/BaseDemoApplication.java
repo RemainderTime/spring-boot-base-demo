d57aae9aaa5bdc09649187f4fad234786db048ca
xiongfeng <2439534726@qq.com>
2024-01-20 10:28:24 +0800

    引入nacos配置中心

--- src/main/java/cn/xf/basedemo/BaseDemoApplication.java
+++ src/main/java/cn/xf/basedemo/BaseDemoApplication.java
@@ -2,8 +2,11 @@ package cn.xf.basedemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@EnableDiscoveryClient
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class BaseDemoApplication {
 
     public static void main(String[] args) {
