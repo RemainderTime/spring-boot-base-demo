cf4c78766b2a8e2e72b17e5151d00b5e66e1dd9c
xiongfeng <2439534726@qq.com>
2024-01-24 16:40:31 +0800

    引入nacos配置中心

--- src/main/java/cn/xf/basedemo/config/MybatisPlusConfig.java
+++ src/main/java/cn/xf/basedemo/config/MybatisPlusConfig.java
@@ -1,7 +1,12 @@
 package cn.xf.basedemo.config;
 
 import com.baomidou.dynamic.datasource.plugin.MasterSlaveAutoRoutingPlugin;
-import com.baomidou.mybatisplus.extension.plugins.pagination.optimize.JsqlParserCountOptimize;
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.autoconfigure.ConfigurationCustomizer;
+import com.baomidou.mybatisplus.core.MybatisConfiguration;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,20 +18,26 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class MybatisPlusConfig {
 
 	/**
-	 * 分页插件
-	 *
-	 * @return PaginationInterceptor
+	 * mybatis_plus插件
 	 */
 	@Bean
-	public PaginationInterceptor paginationInterceptor() {
-		PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
-		// 设置请求的页面大于最大页后操作， true调回到首页，false 继续请求  默认false
-		paginationInterceptor.setOverflow(false);
-		// 设置最大单页限制数量，默认 500 条，-1 不受限制
-		paginationInterceptor.setLimit(500);
-		// 开启 count 的 join 优化,只针对部分 left join
-		paginationInterceptor.setCountSqlParser(new JsqlParserCountOptimize(true));
-		return paginationInterceptor;
+	public MybatisPlusInterceptor mybatisPlusInterceptor() {
+		MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+		//分页插件
+		interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+		// 乐观锁插件
+		interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
+		return interceptor;
+	}
+
+	@Bean
+	public ConfigurationCustomizer configurationCustomizer() {
+		return new ConfigurationCustomizer() {
+			@Override
+			public void customize(MybatisConfiguration configuration) {
+				configuration.setMapUnderscoreToCamelCase(true);
+			}
+		};
 	}
 
 	/**
@@ -39,13 +50,4 @@ public class MybatisPlusConfig {
 		return new MasterSlaveAutoRoutingPlugin();
 	}
 
-	/**
-	 * 乐观锁插件
-	 *
-	 * @return OptimisticLockerInterceptor
-	 */
-	@Bean
-	public OptimisticLockerInterceptor optimisticLockerInterceptor() {
-		return new OptimisticLockerInterceptor();
-	}
 }
