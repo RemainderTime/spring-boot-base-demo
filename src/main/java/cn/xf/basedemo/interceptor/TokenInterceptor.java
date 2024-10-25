6b20b7abb1fe79e3acfe1bef297146f0bd8d3958
xiongfeng <xiongfeng@mail.llzby.top>
2024-10-25 21:45:33 +0800

    ThreadLocal在拦截器后置方法中释放内存

--- src/main/java/cn/xf/basedemo/interceptor/TokenInterceptor.java
+++ src/main/java/cn/xf/basedemo/interceptor/TokenInterceptor.java
@@ -69,5 +69,6 @@ public class TokenInterceptor implements HandlerInterceptor {
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
         HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+        SessionContext.getInstance().clear();
     }
 }
